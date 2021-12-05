a1543d015f8bcbb98be0dfd71304d663b4be80ab
Joey-1970 <paeper@horburg.de>
2021-12-05 12:25:27 +0100

    Create module.php

---
+++ IPS2Amazon_FireTVSplitter/module.php
@@ -0,0 +1,83 @@
+<?
+    // Klassendefinition
+    class IPS2FireTVSplitter extends IPSModule 
+    {
+	// https://github.com/glenndehaan/ikea-tradfri-coap-docs
+	    
+	// Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() 
+        {
+            	// Diese Zeile nicht löschen.
+            	parent::Create();
+		          $this->RegisterPropertyBoolean("Open", false);
+		
+        }
+ 	
+	public function GetConfigurationForm() 
+	{ 
+		$arrayStatus = array(); 
+		$arrayStatus[] = array("code" => 101, "icon" => "inactive", "caption" => "Instanz wird erstellt"); 
+		$arrayStatus[] = array("code" => 102, "icon" => "active", "caption" => "Instanz ist aktiv");
+		$arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
+		$arrayStatus[] = array("code" => 202, "icon" => "error", "caption" => "Kommunikationfehler!");
+		$arrayStatus[] = array("code" => 203, "icon" => "error", "caption" => "Fehlerhafter Key!");
+				
+		$arrayElements = array(); 
+		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv");
+		
+		$arrayActions = array();
+		      	
+ 		return JSON_encode(array("status" => $arrayStatus, "elements" => $arrayElements, "actions" => $arrayActions)); 		 
+ 	}       
+	   
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() 
+        {
+            	// Diese Zeile nicht löschen
+            	parent::ApplyChanges();
+		
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SetStatus(102);
+			}
+			
+		}
+		else {
+			$this->SetStatus(104);
+			
+		}	
+	}
+	
+	public function ForwardData($JSONString) 
+	{
+	 	// Empfangene Daten von der Device Instanz
+	    	$data = json_decode($JSONString);
+	    	$Result = false;
+	 	switch ($data->Function) {
+			case "getDeviceList":
+				$DeviceListArray = array();
+				
+				break;
+		
+		}
+	return $Result;
+	}
+	    
+	// Beginn der Funktionen
+ 
+	
+	private function ConnectionTest()
+	{
+		$Result = false;
+	     	If (Sys_Ping($this->ReadPropertyString("GatewayIP"), 150)) {
+		      	//$this->SendDebug("ConnectionTest", "Angegebene IP ".$this->ReadPropertyString("GatewayIP")." reagiert", 0);
+			$Result = true;
+	      	}
+		else {
+			$this->SendDebug("ConnectionTest", "GatewayIP ".$this->ReadPropertyString("GatewayIP")." reagiert nicht!", 0);
+			IPS_LogMessage("IPS2Tradfi","GatewayIP ".$this->ReadPropertyString("GatewayIP")." reagiert nicht!");
+			$this->SetStatus(202);
+		}
+	return $Result;
+	}
+}
+?>
